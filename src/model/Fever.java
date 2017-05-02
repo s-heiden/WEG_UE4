1ba1a64d557599730c26d7c17e767e2d2a47796f
Stefan Heiden <stefan.w.heiden@gmail.com>
2017-05-02 16:13:14 +0200

    Added fever chart

--- src/model/Fever.java
+++ src/model/Fever.java
@@ -1,20 +1,49 @@
 package model;
 
+import org.primefaces.model.chart.MeterGaugeChartModel;
 import rest.ApiCaller;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.inject.Named;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Created by Katharina on 01.05.2017.
- */
 @ManagedBean(name = "fever")
 @RequestScoped
-public class Fever {
+public class Fever implements Serializable {
 
     private double celsius;
     private double fahrenheit;
+    private MeterGaugeChartModel meterGaugeModel;
+
+    public Fever(){
+        initMeterGaugeModel();
+    }
+
+    @PostConstruct
+    public void init() {
+        createMeterGaugeModel();
+    }
+
+    public MeterGaugeChartModel getMeterGaugeModel() {
+        return meterGaugeModel;
+    }
+
+    private void createMeterGaugeModel() {
+        meterGaugeModel = initMeterGaugeModel();
+        meterGaugeModel.setMin(20);
+        meterGaugeModel.setMax(45);
+        meterGaugeModel.setTitle("Fever");
+        meterGaugeModel.setSeriesColors("66cc66,93b75f,E7E658,cc6666");
+        meterGaugeModel.setGaugeLabel("Degrees");
+        meterGaugeModel.setGaugeLabelPosition("bottom");
+        meterGaugeModel.setShowTickLabels(false);
+//        meterGaugeModel.setLabelHeightAdjust(110);
+//        meterGaugeModel.setIntervalOuterRadius(100);
+    }
 
     public void callWebServiceClient(){
         // calls webserviceclient
@@ -37,6 +66,22 @@ public class Fever {
         this.fahrenheit = fahrenheit;
     }
 
+    public void setMeterGaugeModel(MeterGaugeChartModel meterGaugeModel){
+        this.meterGaugeModel = meterGaugeModel;
+    }
+
+
+    private MeterGaugeChartModel initMeterGaugeModel() {
+        List<Number> intervals = new ArrayList<Number>();
+        intervals.add(20);
+        intervals.add(25);
+        intervals.add(30);
+        intervals.add(35);
+        intervals.add(40);
+        intervals.add(45);
+
+        return new MeterGaugeChartModel(celsius, intervals);
+    }
 
 
 }
---
+++ src/utility/Config.java
@@ -0,0 +1,8 @@
+package utility;
+
+public class Config {
+    public static final String REST_INDEX_URL = "http://localhost:7777/rest/items/vital_data/history";
+    public static final String REST_ADD_URL = "http://localhost:7777/rest/items/vital_data/add";
+    public static final String REST_SHOW_URL = "http://localhost:7777/rest/items/vital_data/";
+    private Config() {}
+}
